5a16e9c18b818f34ce8bec2b66c5d7d4981910e7
codedbyswaliha <swalihachoudhari4@gmail.com>
2026-07-30 12:15:53 +0530

    Delete lecture 03 directory

--- lecture 03/movie_list_manager.py
+++
@@ -1,8 +0,0 @@
-print("Enter your 3 fav movies:")
-movie1=input("Movie 1: ")
-movie2=input("Movie 2: ")
-movie3=input("Movie 3: ")
-movie=[movie1,movie2,movie3]
-print("Your movies:",movie)
-movie.sort()
-print("Sorted movies: ",movie)
